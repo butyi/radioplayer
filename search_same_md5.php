701f81fee1ce247a21588d067f04a6921890a3af
Janos BENCSIK <git@butyi.hu>
2021-02-17 08:35:04 +0100

    same search added

---
+++ search_same_md5.php
@@ -0,0 +1,35 @@
+<?php
+
+$command="find music/ -type f -exec md5sum {} + | sort -k 2 > ./music_md5.txt";
+echo "Execution of ".$command." (it takes minutes)\n";
+system($command);
+
+echo "Search same contents.\n";
+$f=array();
+foreach(file("./music_md5.txt") as $l){
+  $l=trim($l);
+  //df2342de5846a9349fc3f48c19d772ca  ./analogue/2 Unlimited - Do What I Like.mp3
+  $a=explode("  ",$l);
+  if(is_array($a) && count($a)==2){
+    $hash=$a[0];
+    $file=$a[1];
+    $f[$hash][]=$file;
+  } else {
+    echo "Line cannot be exploded '$l'\n";
+    exit(2);
+  }
+}
+
+foreach($f as $hash => $files){
+  if(count($files)==1)unset($f[$hash]);
+}
+
+//print_r($f);
+if(0 < count($f)){
+  $reportfile="report_music_md5.txt";
+  echo "There are ".count($f)." files with same content. See report in $reportfile\n";
+  file_put_contents($reportfile,print_r($f,true));
+} else {
+  echo "There is no files with same content.\n";
+}
+?>
